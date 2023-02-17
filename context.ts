3945f37b14bf5d40771f81bdc631ec679cbf2ab3
xArthurDev <arthurrguimaraes12@gmail.com>
2023-02-17 17:22:49 -0300

    feat: create prisma mockup setup

---
+++ context.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+
+export type Context = {
+  prisma: PrismaClient;
+};
+
+export type MockContext = {
+  prisma: DeepMockProxy<PrismaClient>;
+};
+
+export const createMockContext = (): any => {
+  return {
+    prisma: mockDeep<PrismaClient>(),
+  };
+};
